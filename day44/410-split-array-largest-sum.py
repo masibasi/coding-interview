cc4a57810c391e7406ecdca1e81c9c45f66b3158
masibasi <jimin_lee98@naver.com>
2026-01-24 23:35:33 -0800

    day44-410

--- day44/410-split-array-largest-sum.py
+++ day44/410-split-array-largest-sum.py
@@ -1,1 +1,28 @@
 # https://leetcode.com/problems/split-array-largest-sum/
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        low, high = max(nums), sum(nums)
+
+        def can_split(max_sum):
+            cur_sum = 0
+            chunk = 1
+            for num in nums:
+                if cur_sum + num > max_sum:
+                    chunk += 1
+                    cur_sum = num
+                else:
+                    cur_sum += num
+            return chunk <= k 
+
+        ans = high
+        while low <= high:
+            mid = (low + high) // 2
+            if can_split(mid):
+                ans = mid
+                high = mid -1
+            else:
+                low = mid + 1
+
+        return ans
