776600254aa4baeddbb1834a8f10baf63535cbb8
Jimin Lee <leejimin@usc.edu>
2026-02-02 20:39:11 -0800

    day47

--- day47/047-permutations-ii.py
+++ day47/047-permutations-ii.py
@@ -1,1 +1,45 @@
-# https://leetcode.com/problems/permutations-ii/
+# https://leetcode.com/problems/permutations-ii/description/
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+
+        nums.sort()
+
+        def dfs(num_list, perm):
+            if len(num_list) == 0:
+                ans.add(tuple(perm))
+
+            for i, num in enumerate(num_list):
+                dfs(num_list[:i] + num_list[i+1:], perm + [num])
+
+
+        dfs(nums, [])
+
+        return list(ans)
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        visited = [False] * len(nums)
+        ans = []
+        def dfs(path):
+            if len(path) == len(nums):
+                ans.append(path[:])
+
+            for i, num in enumerate(nums):
+                if i > 0 and nums[i] == nums[i-1] and not visited[i-1]:
+                    continue
+
+                if not visited[i]:
+                    path.append(num)
+                    visited[i] = True
+
+                    dfs(path)
+
+                    path.pop()
+                    visited[i] = False
+
+        dfs([])
+
+        return ans
--- day47/051-n-queens.py
+++ day47/051-n-queens.py
@@ -1,1 +1,39 @@
 # https://leetcode.com/problems/n-queens/
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        if n == 1:
+            return [["Q"]]
+
+        board = [["."] * n for _ in range(n)]
+        ans = []
+
+        cols = set()      # 열
+        diag1 = set()     # 우하향 대각선 (r - c)
+        diag2 = set()     # 우상향 대각선 (r + c)
+
+        def dfs(row):
+            # Base Case: 마지막 행까지 무사히 퀸을 다 채웠다면 성공!
+            if row == n:
+                # 현재 보드 상태를 문자열 리스트로 변환해서 저장
+                ans.append(["".join(r) for r in board])
+                return
+
+            for col in range(n):
+                if col in cols or (row - col) in diag1 or (row + col) in diag2:
+                    continue
+
+                cols.add(col)
+                diag1.add(row-col)
+                diag2.add(row+col)
+                board[row][col] = "Q"
+                dfs(row+1)
+
+                cols.remove(col)
+                diag1.remove(row-col)
+                diag2.remove(row+col)
+                board[row][col] = "."
+
+        dfs(0)
+
+        return ans
